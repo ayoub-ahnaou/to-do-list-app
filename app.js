6fb05a160e8f01192fb1544a2c1f3608284e0239
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-23 23:34:57 +0100

    rebuild the app with list attributs

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const task_input = document.getElementById("task_input");
-const task_btn = document.getElementById("task_btn");
+const tasks = document.getElementById("tasks");
 
-const tasks = document.getElementById("taskss");
+const input_value = document.getElementById("task_input");
+const btn_add = document.getElementById("task_btn");
 
-task_btn.addEventListener("click" , () => {
-    if(task_input.value != ""){
-        tasks.innerHTML += `
-           <div class="task">
-                <input type="checkbox" id="task_done" name="task_title">
-                <label id="task_title" class="" htmlFor="task_done">${task_input.value}</label>
-                <p id="task_delete">&#x2715;</p>
-            </div> 
-        `;
-        task_input.value = "";
-    }
-})
+btn_add.addEventListener("click", () => {
+  if (input_value.value != "") {
+    add_task(input_value.value);
+    
+    input_value.value = "";
+    input_value.focus();
+  }
+});
+
+function add_task(title){
+}
