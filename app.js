106856f1e62cf23c028503c9e9b6bf20a8919ec4
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-09-24 01:21:09 +0100

    To-Do list app developpement is done

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const tasks = document.getElementById("tasks");
+const field_value = document.getElementById("task_input");
+const addBtn = document.getElementById("task_btn");
+const tasks = document.querySelector(".tasks");
 
-const input_value = document.getElementById("task_input");
-const btn_add = document.getElementById("task_btn");
-
-btn_add.addEventListener("click", () => {
-  if (input_value.value != "") {
-    add_task(input_value.value);
-    
-    input_value.value = "";
-    input_value.focus();
+addBtn.addEventListener("click", () => {
+  if (field_value.value != "") {
+    let item = document.createElement("div");
+    item.classList.add("item");
+    item.innerHTML += `
+        <label>${field_value.value}</label>
+        <div class="btns">
+            <p class="markdone">finish</p>
+            <p class="delete">delete</p>
+        </div>
+        `;
+    tasks.appendChild(item);
+    field_value.value = "";
+    field_value.focus();
   }
 });
 
-function add_task(title){
-}
+tasks.addEventListener("click", (e) => {
+    if(e.target.classList.contains("markdone")){
+        e.target.parentElement.parentElement.classList.toggle("checked");
+    }
+});
+
+tasks.addEventListener("click", (e) => {
+    if(e.target.classList.contains("delete")){
+        e.target.parentElement.parentElement.remove();
+    }
+})
+
